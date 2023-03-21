6f09624f12aa77effec6003cdc8d40139d9e337b
2beyanga <cindytumaini06@gmail.com>
2023-03-21 02:45:57 -0700

    check for alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase then a new line ten times
+ *
+ * Return : Always 0
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+
+			_putchar ('\n');
+		}
+	}
+}
