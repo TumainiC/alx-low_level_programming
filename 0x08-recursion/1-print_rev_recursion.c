bdd0421a526b32dd023dc8d8cb08bfe67315e618
2beyanga <cindytumaini06@gmail.com>
2023-04-06 01:28:43 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+include "main.h"
+
+/**
+ * void _print_rev_recursion - print a string backwards
+ * @s:string
+ * Return:void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursions(s + 1);
+		_putchar('\n');
+	}
+}
