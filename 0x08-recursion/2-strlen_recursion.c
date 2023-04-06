9001ee12a20eb959a3265dc5e5029d97fa5ffbda
2beyanga <cindytumaini06@gmail.com>
2023-04-06 02:36:47 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recurion - prints length of a string
+ * @s: string
+ * Return: int
+*/
+
+int _strlen_recursion(char *s)
+{
+	int 1 = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recurion(s + 1);
+	}
+	return(i);
+}
