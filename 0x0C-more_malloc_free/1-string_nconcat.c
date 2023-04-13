722002daef834c34df8f38aea3ae04ff0b16a3e3
Tumaini <99716141+2beyanga@users.noreply.github.com>
2023-04-13 15:43:32 +0300

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,21 +15,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int i, j, len1, len2;
 
 	i = j = len1 = len2 = 0;
-	/*null string is passed*/
-	if (s1 == NULL)
-		s1 = " ";
-	if (s2 == NULL)
-		s2 = " ";
+	
 	/*counting the length of the string*/
 	while (s1 && s1[len1])
+	{
 		len1++;
+	}
+	
 	while (s2 && s2[len2])
+	{
 		len2++;
+	}
 
 	if (n >= len2)
-		conc = malloc(sizeof(char) * (len + n + 1));
+		conc = malloc(sizeof(char) * (len1 + n + 1));
 	else
-		conc = malloc(sizeof(char) * (len + len2 + 1));
+		conc = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (conc == NULL)
 		return (NULL);
