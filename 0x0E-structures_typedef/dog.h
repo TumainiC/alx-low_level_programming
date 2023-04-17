b31571d914a846f60e449a5f2fec2a768769e923
2beyanga <cindytumaini06@gmail.com>
2023-04-17 07:35:08 -0700

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,26 +1,29 @@
 #ifndef DOG
 #define DOG
 
-/*
- * struct dog - fuction to display the name, trype and age of god
- * @name: name of dog
- * @age: age of dog
- * @owner: owner of dog
+/**
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
  */
+
 struct dog
 {
 	char *name;
-
-	float *age;
-
+	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
 typedef struct dog dog_t;
 
-dog_t *new_dog(char *name, float *age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
 
